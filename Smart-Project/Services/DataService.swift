cc66603d09f4fd96d1ca6ed6e06fa1d6bc0485a1
Omid <omidtavanaei@gmail.com>
2019-04-23 01:47:43 +0430

    Tests TableView completed

--- Smart-Project/Services/DataService.swift
+++ Smart-Project/Services/DataService.swift
@@ -13,12 +13,18 @@ class DataService {
     static let instance = DataService()
     
     private let tests = [
-    Test(title: "تست شخصیت شناسی مایرز-بریگز", imageName: "MBTI.png", discription: "تست تیپ شخصیت و شغل مربوط به آن"),
+    Test(title: "تست شخصیت شناسی مایرز-بریگز MBTI", imageName: "MBTI.png", discription: "تست تیپ شخصیت و شغل مربوط به آن"),
+    Test(title: "تست هوش تصویری Raven", imageName: "IQ.png", discription: "نمره هوش شما از لحاظ پردازش تصویر"),
     Test(title: "تست هوش هیجانی Baron", imageName: "EQ.png", discription: "توانایی شما برای کنترل احساسات درجهت انجام کار"),
     Test(title: "پروفایل DISC", imageName: "DISC.png", discription: "آگاهی نسبت به میزان انگیزه برای انجام کاری"),
-//    Test(title: "<#T##String#>", imageName: "<#T##String#>", discription: "<#T##String#>"),
-//    Test(title: "<#T##String#>", imageName: "<#T##String#>", discription: "<#T##String#>"),
-//    Test(title: "<#T##String#>", imageName: "<#T##String#>", discription: "<#T##String#>")
+    Test(title: "تست سبک انگیزشی", imageName: "DMSI.png", discription: "انجام چه کاری باعث ایجاد انگیزه درشما می شود"),
+    Test(title: "تست توانایی‌های شناختی", imageName: "Cognitive.png", discription: "برآیند توانایی شناختی شما در مسایل"),
+    Test(title: "تست شخصیت شناسی Hartman", imageName: "Hartman.png", discription:"تعیین رنگ شخصیت شما و معانی آن"),
+    Test(title: "تست شخصت شناسی Neo", imageName: "Neo.png", discription: "تاثیر ۵ عامل اصلی در شخصیت شما"),
+    Test(title: "تست سبک حل تعارض TKI", imageName: "TKI.png", discription: "سبک حل تعارض بین شما و بقیه افراد چگونه است"),
+    Test(title: "سبک برخورد با استرس CSI", imageName: "CSI.png", discription: "با نحوه مدیریت استرس خود آشنا شوید"),
+    Test(title: "شناسایی لنگرگاه های مسیر شغلی", imageName: "Anchor.png", discription: "برآیند انتظارات شما از شغل خود"),
+    Test(title: "تست سلامت بهداشت عمومی", imageName: "GHQ.png", discription: "وضعیت سلامت عمومی شما")
     ]
     
     func getTests() -> [Test] {
